3f9bc6f2b0b1086f0725c559171b08cb652bb5fd
jiwoo0204 <sally12234@naver.com>
2023-02-26 16:51:06 +0900

    mybatis

--- src/test/java/com/example/kdtjpaproject/JDBCTest.java
+++ src/test/java/com/example/kdtjpaproject/JDBCTest.java
@@ -2,6 +2,10 @@ package com.example.kdtjpaproject;
 
 import org.junit.jupiter.api.Test;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,6 +13,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 @Slf4j
+@SpringBootTest
 public class JDBCTest {
     static final String JDBC_DRIVER = "org.h2.Driver";
     static final String DB_URL = "jdbc:h2:~/test";
@@ -18,6 +23,9 @@ public class JDBCTest {
     static final String DROP_TABLE_SQL = "DROP TABLE customers IF EXISTS";
     static final String CREATE_TABLE_SQL = "CREATE TABLE customers(id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))";
     static final String INSERT_SQL = "INSERT INTO customers (id, first_name, last_name) VALUES(1, 'honggu', 'kang')";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void jdbc_sample() {
@@ -48,6 +56,18 @@ public class JDBCTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void jdbcTemplate_sample() {
+        jdbcTemplate.update(DROP_TABLE_SQL);
+        jdbcTemplate.update(CREATE_TABLE_SQL);
+        log.info("CREATED TABLE USING JDBC TEMPLATE");
+
+        String fullName = jdbcTemplate.queryForObject(
+                "SELECT * FROM customers WHERE id = 1",
+                (resultSet, i) -> resultSet.getString("first_name") + " " + resultSet.getString("last_name")
+        );
     }
 
 }
