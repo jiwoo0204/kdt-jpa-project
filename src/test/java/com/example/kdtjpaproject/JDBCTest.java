a814b31eb0def1e23cd96d707b06c17447ed5526
jiwoo0204 <sally12234@naver.com>
2023-02-26 12:08:46 +0900

    Initial commit

---
+++ src/test/java/com/example/kdtjpaproject/JDBCTest.java
@@ -0,0 +1,9 @@
+package com.example.kdtjpaproject;
+import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JDBCTest {
+    static final String JDBC_DRIVER = "org.h2.Driver";
+}
---
+++ src/test/java/com/example/kdtjpaproject/KdtJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdtjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdtJpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
