8e2bf1f2bcca252ff1612a1090306f2f8677eecf
jiwoo0204 <sally12234@naver.com>
2023-02-26 12:17:01 +0900

    init 2

--- src/test/java/com/example/kdtjpaproject/JDBCTest.java
+++ src/test/java/com/example/kdtjpaproject/JDBCTest.java
@@ -6,4 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JDBCTest {
     static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+    static final String USER = "sa";
+    static final String PASS = "";
 }
