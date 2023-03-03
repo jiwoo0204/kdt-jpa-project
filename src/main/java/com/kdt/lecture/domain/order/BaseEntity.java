ca60c6e19c4c6210f605d8da4d94a52db70f3e7f
jiwoo0204 <sally12234@naver.com>
2023-03-03 23:36:17 +0900

    Order API - SAVE

--- src/main/java/com/kdt/lecture/domain/order/BaseEntity.java
+++ src/main/java/com/kdt/lecture/domain/order/BaseEntity.java
@@ -1,9 +1,13 @@
 package com.kdt.lecture.domain.order;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity {
     @Column(name = "created_by")
