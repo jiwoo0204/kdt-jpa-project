af8d3c769881c49a45eff19ee1881ecf0df3f006
jiwoo0204 <sally12234@naver.com>
2023-03-03 17:50:02 +0900

    extends BaseEntity

--- src/main/java/com/kdt/lecture/domain/order/OrderItem.java
+++ src/main/java/com/kdt/lecture/domain/order/OrderItem.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 @Table(name = "order_item")
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
