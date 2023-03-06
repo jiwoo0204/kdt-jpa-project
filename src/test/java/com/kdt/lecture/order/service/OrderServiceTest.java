124ac18ba143b6d4c15b60e389625368f3c857b3
jiwoo0204 <sally12234@naver.com>
2023-03-06 20:45:49 +0900

    test class

--- src/test/java/com/kdt/lecture/order/service/OrderServiceTest.java
+++ src/test/java/com/kdt/lecture/order/service/OrderServiceTest.java
@@ -34,7 +34,6 @@ class OrderServiceTest {
     String uuid = UUID.randomUUID().toString();
 
     @BeforeEach
-    @Test
     void setUp () {
         // Given
         OrderDto orderDto = OrderDto.builder()
